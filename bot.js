5e88faf8a98367573656919532664b2d681b6cbe
jariz <jarizw@gmail.com>
2014-10-30 00:06:07 +0100

    Remove scope, PM monitor works, still needs handling pm's.

--- bot.js
+++ bot.js
@@ -1,82 +1,89 @@
-var ChannelBot = function (reddit, chalk, yaml, fs, figures, storage, youtube) {
-    this.version = "ChannelBot 2.0.0.0";
-
-    var config, reddit = new reddit(this.version);
-
-
-    this.init = function () {
-
-        process.stdout.write(chalk.bold.dim(this.version) + "\n");
-        process.stdout.write(chalk.dim("By Jari Zwarts") + "\n\n");
-
-        try {
-            //start the init
-
-            this.loading("configuration");
-            config = yaml.safeLoad(fs.readFileSync("config.yaml", "utf-8"));
-            this.loadingOK();
-
-            this.loading("database");
-            storage = new storage();
-            this.loadingOK();
-
-            this.loading("reddit");
-            reddit.login(config.username, config.password);
-            this.loadingOK();
-
-            this.loading("youtube");
-            youtube = new youtube(config.yt_key);
-            this.loadingOK();
-        } catch (e) {
-            this.loadingFail(e);
-        }
-
-        //all systems go
-        this.monitor.start();
-    };
-
-    this.monitor = function() {
-        this.start = function() {
-            this.pms();
-        }
-
-        this.pms = function() {
-
-        };
-    }();
-
-    this.loading = function (what) {
-        process.stdout.write(chalk.bold("Loading " + what + "... "));
-    };
-
-    this.loadingOK = function () {
-        process.stdout.write(chalk.green(figures.tick) + " OK!\n");
-    };
-
-    this.loadingFail = function (error) {
-        process.stdout.write(chalk.red(figures.cross) + " Fail\n");
-        if (error) throw error;
-        else process.exit(1);
-    };
-
-    this.error = function (msg) {
-        if (!config || (config && config.log.error))
-            process.stdout.write(chalk.red(msg) + "\n");
-    };
-
-    this.info = function (msg) {
-        if (!config || (config && config.log.info))
-            process.stdout.write(chalk.blue(msg) + "\n");
-    };
+var reddit = require("./nodewhal/nodewhal"),
+    chalk = require("chalk"),
+    yaml = require("js-yaml"),
+    fs = require("fs"),
+    figures = require("figures"),
+    storage = require("./storage"),
+    youtube = require("youtube-get"),
+    version = "ChannelBot 2.0",
+    config;
 
 
-    this.init();
-}(
-    require("./nodewhal/nodewhal"),
-    require("chalk"),
-    require("js-yaml"),
-    require("fs"),
-    require("figures"),
-    require("./storage"),
-    require("youtube-get")
-);
+init = function () {
+
+    process.stdout.write(chalk.bold.dim(version) + "\n");
+    process.stdout.write(chalk.dim("By Jari Zwarts") + "\n\n");
+
+    try {
+        //start the init
+
+        loading("configuration");
+        config = yaml.safeLoad(fs.readFileSync("config.yaml", "utf-8"));
+        loadingOK();
+
+        loading("database");
+        storage = new storage();
+        loadingOK();
+
+        loading("youtube");
+        youtube = new youtube(config.yt_key);
+        loadingOK();
+
+        loading("reddit");
+        reddit = new reddit(version);
+        reddit.login(config.username, config.password).then(function() {
+            loadingOK();
+
+            //all systems go
+            monitor.start();
+
+        });
+    } catch (e) {
+        loadingFail(e);
+    }
+};
+
+monitor = {
+    start: function () {
+        setInterval(this.pms, 2500) || this.pms();
+    },
+
+    pms: function () {
+        var unread = reddit.listing('/message/unread').then(function(data) {
+            var keys = Object.keys(data);
+            keys.length && keys.forEach(function(key) {
+                var pm = data[key];
+                //handle pm
+                this.info("received: "+pm.subject);
+            })
+        });
+
+    }
+};
+
+loading = function (what) {
+    process.stdout.write(chalk.bold("Loading " + what + "... "));
+};
+
+loadingOK = function () {
+    process.stdout.write(chalk.green(figures.tick) + " OK!\n");
+};
+
+loadingFail = function (error) {
+    process.stdout.write(chalk.red(figures.cross) + " Fail\n");
+    if (error) throw error;
+    else process.exit(1);
+};
+
+error = function (msg) {
+    if (!config || (config && config.log.indexOf('error') != -1))
+        process.stdout.write(chalk.red(msg) + "\n");
+};
+
+info = function (msg) {
+    if (!config || (config && config.log.indexOf('info') != -1))
+        process.stdout.write(chalk.blue(msg) + "\n");
+};
+
+
+init();
