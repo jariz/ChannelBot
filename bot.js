47e81520689f694589d24e08ad2bb602d29dc2d4
jariz <jarizw@gmail.com>
2014-11-03 00:57:09 +0100

    Adding: validation & parsing sorta working

--- bot.js
+++ bot.js
@@ -6,11 +6,15 @@ var reddit = require("./nodewhal/nodewhal"),
     storage = require("./storage"),
     youtube = require("youtube-get"),
     log = require("./log"),
+    validate = require("validate-obj"),
     version = "ChannelBot 2.0",
+    redditurl = "http://www.reddit.com",
     config;
 
 
 init = function () {
+
+    validators();
 
     process.stdout.write(chalk.bold.dim(version) + "\n");
     process.stdout.write(chalk.dim("By Jari Zwarts") + "\n\n");
@@ -38,29 +42,55 @@ init = function () {
             //all systems go
             monitor.start();
 
+        }, function() {
+            log.loadingFail();
         });
     } catch (e) {
         log.loadingFail(e);
     }
 };
 
-markPMRead = function(id) {
-    reddit.post("/api/read_message", { form: {
-        "id": id,
+markPMRead = function(name) {
+    reddit.post(redditurl+"/api/read_message", { form: {
+        "id": name,
         "uh": reddit.session.modhash
     }});
 };
 
-sendPM = function (subject, message) {
-    reddit.post("/api/", {
+sendPM = function (subject, message, to) {
+    reddit.post(redditurl+"/api/compose", {
         form: {
-        "api_type": "hi"
-    }
+            "api_type": "json",
+            "subject": subject,
+            "text": message,
+            "to": to,
+            "uh": reddit.session.modhash
+        }
     });
 };
 
 /**
- * monitor monitors incoming PM's, uploads
+ * validators provides some extra validation functions that validate-obj doesn't have
+ */
+validators = function() {
+    validate.register("isAlphaNum", function(value, params) {
+        if(params && params[0]) var additional = params[0];
+        else additional = "";
+
+        var reg = new RegExp("^[a-zA-Z0-9"+additional+"]+$")
+        return /^[a-zA-Z0-9]+$/.test(value);
+    }, function(name) {
+        return name + " isn't a alphanumeric string"
+    });
+//    validate.register("noWhiteSpaces", function(value) {
+//        return !/ /.test(value);
+//    }, function(name) {
+//        return name + " contains whitespaces";
+//    });
+}
+
+/**
+ * monitor monitors for incoming PM's, uploads
  */
 monitor = {
     start: function () {
@@ -68,38 +98,99 @@ monitor = {
     },
 
     pms: function () {
-        var unread = reddit.listing('/message/unread').then(function(data) {
-            var keys = Object.keys(data);
-            keys.length && keys.forEach(function(key) {
-                var pm = data[key];
-                //handle pm
-                handler.PM(pm);
-            })
-        });
-
+        try {
+            var unread = reddit.listing('/message/unread').then(function(data) {
+                var keys = Object.keys(data);
+                keys.length && keys.forEach(function (key) {
+                    var pm = data[key];
+                    //handle pm
+                    handler.PM(pm);
+                })
+            });
+        } catch(e) {
+            log.error("Error in monitor.pm thread", e);
+        }
     }
 };
 
+/**
+ * handler processes actions the monitor picks up
+ */
 handler = {
     PM: function(pm) {
 
-        if(pm.was_comment) {
-            log.debug("Ignored a comment reply from "+pm.author);
-            markPMRead(pm.id);
-            return;
+        var markReadAndRespond = function(subject, message) {
+            log.info("Responded with '"+subject+"'")
+            sendPM(subject, message, pm.author);
+            markPMRead(pm.name);
+        };
+
+        try {
+            if(pm.was_comment) {
+                log.debug("Ignored a comment reply from "+pm.author);
+                markPMRead(pm.name);
+                return;
+            }
+
+            log.info("Got a message from '{0}' with the subject '{1}'!".format(pm.author, pm.subject));
+
+            log.debug("\n----------------\n"+pm.body+"\n----------------");
+            log.debug("Processing message:")
+            log.debug(figures.pointer+" Parsing...");
+
+            //parse contents
+            try {
+                var message = yaml.safeLoad(pm.body);
+            } catch(e) {
+                markReadAndRespond("Unable to parse your message", "Your message contains invalid YAML. For more info, read the [API docs](http://www.reddit.com/r/ChannelBot/wiki/api) and [YAML formatting](https://en.wikipedia.org/wiki/YAML)");
+                return;
+            }
+
+            switch(pm.subject.toLowerCase()) {
+                case "add":
+                    log.debug(figures.pointer+" Validating fields");
+
+                    var validation = {
+                        subreddit: [validate.required, validate.isString, validate.minLength([1]), validate.isAlphaNum(["_"])],
+                        channel_id: [validate.isString, validate.minLength([24]), validate.maxLength([24])],
+                        channel: [validate.isString, validate.minLength([1]), validate.isAlphaNum],
+
+                        selfCrossValidators: function(message) {
+                            if(!("channel" in message && "channel_id" in message)) return "Please provide either 'channel' or 'channel_id'"
+                        }
+                    };
+
+                    var errors = validate.hasErrors(message);
+                    if(errors) {
+                        var reply = "The following error{0} occurred while validating your message: \n\n".format(errors.length > 1 ? "s" : "");
+                        errors.forEach(function(error) {
+                            reply += "\n\n- {0}\n\n".format(error);
+                        });
+                        reply += "\n\nDon't forget to read the [docs](http://www.reddit.com/r/ChannelBot/wiki/api)";
+                        markReadAndRespond("Unable to add channel", reply);
+                        return;
+                    }
+
+                    markReadAndRespond("yay", "Successful. I should add something but I'm not programmed to do so yet");
+
+                    break;
+                default:
+                    markReadAndRespond("Invalid subject.", "Subject needs to be one of the following:\n\n'add', 'list' OR 'remove'.")
+                    break;
+            }
+        } catch(e) {
+            log.error("Error while processing PM!", e);
+            markReadAndRespond("Internal error", "Couldn't process your message because of a internal error. Please contact the administrator of this bot.");
         }
-
-        switch(pm.subject.toLowerCase()) {
-            case "add":
-
-                break;
-            default:
-
-                break;
-        }
-
     }
 }
+
+String.prototype.format = function() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) {
+        return typeof args[number] != 'undefined' ? args[number] : match
+    });
+};
 
 
 init();
--- log.js
+++ log.js
@@ -1,5 +1,6 @@
 var chalk = require("chalk"),
-    figures = require("figures");
+    figures = require("figures"),
+    logerrors = require("log-errors");
 
 var Log = {
     config: undefined,
@@ -18,19 +19,27 @@ var Log = {
         else process.exit(1);
     },
 
-    error: function (msg) {
-        if (!config || (config && config.log.indexOf('error') != -1))
-            process.stdout.write(chalk.red(msg) + "\n");
+    error: function (msg, error) {
+        if (!this.config || (this.config && this.config.log.indexOf('error') != -1)) {
+            process.stdout.write(chalk.red(figures.circleCross + " " + msg) + "\n");
+            if(error && (!this.config || (this.config && this.config.log.indexOf('debug') != -1))) {
+                logerrors.development(error);
+            } else if(error) {
+                logerrors.production(error);
+            }
+        }
+
+
     },
 
     info: function (msg) {
-        if (!config || (config && config.log.indexOf('info') != -1))
-            process.stdout.write(chalk.blue(msg) + "\n");
+        if (!this.config || (this.config && this.config.log.indexOf('info') != -1))
+            process.stdout.write(figures.info + " " + chalk.blue(msg) + "\n");
     },
 
-    debug: function(debug) {
-        if (!config || (config && config.log.indexOf('debug') != -1))
-            process.stdout.write(chalk.blue(msg) + "\n");
+    debug: function(msg) {
+        if (!this.config || (this.config && this.config.log.indexOf('debug') != -1))
+            process.stdout.write(chalk.green(figures.circleCircle+ " " + msg) + "\n");
     }
 };
 
